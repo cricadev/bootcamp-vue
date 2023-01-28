726f58495497d6ad4f696d0bcb04db2118e5ffd0
CricaDev <cristiancasallas998@gmail.com>
2023-01-28 08:54:08 -0500

    quiz header and quizlug implemented

---
+++ quiz/components/QuizHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <header class="mt-5">
+      <h2 class="text-2xl">Question 1/3</h2>
+      <div class="bar w-72 h-12 border-green-500 border-[2px]">
+        <div class="completion"></div>
+      </div>
+    </header>
+  </div>
+</template>
+<script setup>
+const { questionStatus } = defineProps(["questionStatus"]);
+</script>
---
+++ quiz/pages/quiz/[...slug].vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <QuizHeader :question-status="questionStatus"></QuizHeader>
+    <Question :question="quiz.questions[currentQuestionIndex]"></Question>
+  </div>
+</template>
+
+<script setup>
+import quizes from "../../quizes.json";
+const route = useRoute();
+const quizId = parseInt(route.params.id);
+const quiz = quizes.find((q) => q.id == quizId);
+const currentQuestionIndex = ref(0);
+const questionStatus = `${currentQuestionIndex.value}/${quiz?.questions.length}`;
+</script>
+
+<style scoped>
+.completion {
+  width: 33%;
+  height: 100%;
+  background-color: green;
+}
+</style>
