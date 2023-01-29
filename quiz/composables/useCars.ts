2f81dd43daff0537d789ebbe3c6bf4de75205aa4
CricaDev <cristiancasallas998@gmail.com>
2023-01-28 22:50:33 -0500

    staging now only one prpoject showing

--- quiz/composables/useCars.ts
+++
@@ -1,4 +0,0 @@
-import cars from "../data/cars.json";
-export const useCars = () => {
-  return cars;
-};
--- quiz/error.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>404 NOT FOUND</div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
